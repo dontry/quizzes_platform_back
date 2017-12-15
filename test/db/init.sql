30487070f76712e295b23a54c035aa3fc1e685a0
dontry <mccoy018@gmail.com>
2017-12-15 17:17:31 +1100

    create travis.sql schema

--- test/db/init.sql
+++
@@ -1,2 +0,0 @@
-# DROP TABLE
-DROP TABLE answer, question, quiz;
---
+++ test/db/travis.sql
@@ -0,0 +1,3 @@
+# DROP TABLE
+CREATE DATABASE IF NOT EXISTS `quizzes_platform_test` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `quizzes_platform_test`;
